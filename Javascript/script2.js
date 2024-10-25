390011a07acb11a4dea9f53b60b2ad89127c6090
MM19240 <mm19240@ues.edu.sv>
2024-10-24 21:43:58 -0600

    Add files via upload

---
+++ Javascript/script2.js
@@ -0,0 +1,4 @@
+document.getElementById("insertar").addEventListener("acceder", function() {
+    document.getElementById("descripcion").textContent = "¡Se ha insertado el botòn correctamente!"; 
+  });
+  
