4b4ebee3126e569b3ed20692bda1d7e572fed90f
Eliel Abisai Avelar Jaimes <elielavelar@gmail.com>
2024-10-24 21:56:16 -0600

    Modificación de botón secundario

--- Javascript/script2.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("insertar").addEventListener("acceder", function() {
-    document.getElementById("descripcion").textContent = "¡Se ha insertado el botòn correctamente!"; 
-  });
-  
---
+++ js/script2.js
@@ -0,0 +1,4 @@
+document.getElementById("insertar").addEventListener("click", function() {
+    document.getElementById("descripcion").textContent = "¡Ahora se ha dado click sobre este otro botón!"; 
+  });
+  
